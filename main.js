2c3757a8e133e0c61703b07bfef803e1b85ef574
renu <gmkrenuka534@gmail.com>
2018-06-25 02:08:28 -0700

    first commit

--- main.js
+++ main.js
@@ -13,12 +13,29 @@ function loadJson(file,callback){
 loadJson("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
+  //fun definitions
+  fun_career(data.career);
 fun_education(data.education);
+fun_acheivements(data.acheivements);
+fun_skills(data.skills);
 });
 var div2=document.getElementById("renuka");
+//career fun_education
+function fun_career(careerobj){
+  var careerHeading=document.createElement("h2");
+  careerHeading.textContent="career objective";
+  div2.appendChild(careerHeading);
+  var hr=document.createElement("hr");
+  div2.appendChild(hr);
+  var careerInfo=document.createElement("p");
+  careerInfo.textContent=careerobj.Info;
+  div2. appendChild(careerInfo);
+
+}
 function fun_education(edu){
   var h1=document.createElement("h2");
   h1.textContent="education qualification";
+  div2.appendChild(h1);
   var hr=document.createElement("hr");
   div2.appendChild(hr);
   var list=document.createElement("ul");
@@ -35,4 +52,36 @@ var listItem=document.createElement("p");
 listItem.textContent=edu[i].data;
 list.appendChild(listItem);
 }
+}
+function fun_acheivements(ach){
+
+  var h1=document.createElement("h2");
+  h1.textContent="acheivements";
+  div2.appendChild(h1);
+
+for(var i=0; i<ach.length; i++)
+{
+  var listItem=document.createElement("p");
+  listItem.innerHTML=ach[i].name;
+  div2.appendChild(listItem);
+
+}
+}
+function fun_skills(tech){
+  var heading=document.createElement("h2");
+  heading.textContent="Technical skills";
+  div2.appendChild(heading);
+  var hr=document.createElement("hr");
+  heading.appendChild(hr);
+  var tr="";
+
+  var table=document.createElement("table");
+  table.border="1";
+  div2.appendChild(table);
+  for(var i=0; i<tech.length; i++){
+    tr+="<tr><td>"+tech[i].name+"</td><td>"+tech[i].data+"</td></tr>";
+
+  }
+  table.innerHTML=tr;
+
 }
